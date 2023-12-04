9ef09963bb55f3a6d13da4961521e197c9888871
miaomiaonana <79128478+miaomiaonana@users.noreply.github.com>
2023-12-04 19:02:19 +1100

    Add files via upload

---
+++ Smart-Contracts/ledgermon.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Pokemon is ERC721 {
+    using Strings for uint256;
+    uint256 private _nextTokenId = 1; //start at 
+    uint256 public cost = 1 ether;
+    string public uriSuffix = ".json";
+
+    constructor() ERC721("Pokemon", "PKM") {}
+
+    // Set the base location of the token URI
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://teal-key-jaguar-194.mypinata.cloud/ipfs/QmSadmgBdqNMnUiD3LHJq5PZTeHnNfyFqdLTfdYyHUexAh/";
+    }
+
+    // Concatenate the baseURI + tokenID + .json
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireOwned(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString(), uriSuffix) : "";
+    }
+
+    // Buy a Ledgemon!
+    function MINT() public payable {
+        require(msg.value >= cost, "Insufficient funds!");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+  function withdraw() public {
+    // =============================================================================
+    (bool hs1, ) = payable(0x1Fa036B51FB30d460F7F1Aa5Ca0C6aaC724920Af).call{value: address(this).balance * 25 / 100}("");
+    require(hs1);
+    (bool hs2, ) = payable(0x1Fa036B51FB30d460F7F1Aa5Ca0C6aaC724920Af).call{value: address(this).balance * 25 / 100}("");
+    require(hs2);
+    (bool hs3, ) = payable(0x1Fa036B51FB30d460F7F1Aa5Ca0C6aaC724920Af).call{value: address(this).balance * 25 / 100}("");
+    require(hs3);
+    (bool hs4, ) = payable(0x1Fa036B51FB30d460F7F1Aa5Ca0C6aaC724920Af).call{value: address(this).balance * 25 / 100}("");
+    require(hs4);
+  }
+
+}
